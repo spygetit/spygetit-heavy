2ecac45e371c1c1a7d35040901f19f04f3cd3181
yohann <yohann.goardou@gmail.com>
2022-06-03 00:16:59 +0200

    Run both server concurrently

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from threading import Thread
 import frontend
 import backend
 
-
 if __name__ == "__main__":
-    bck = Thread(target=backend.start)
+    bck = Thread(target=backend.start, daemon=True)
     bck.start()
     frontend.start()
     bck.join()
